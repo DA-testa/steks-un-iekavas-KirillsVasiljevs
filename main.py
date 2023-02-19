7db8a376ce67d58696f0dceaef09c7e33e9fde4d
Kirills <kirills.vasiljevs@edu.rtu.lv>
2023-02-19 22:01:35 +0200

    fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
-        if next in "I([{":
+        if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
         if next in ")]}":
             if not are_matching(opening_brackets_stack[-1].char, next):
